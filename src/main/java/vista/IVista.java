531ce0f2159cbdca6c33ffa6f4c3a2dd6498d00d
Issa-hub14 <isabellaquijano1@gmail.com>
2026-05-04 16:02:03 -0500

    Creacion de la interfaz IVista en el paquete vista

---
+++ src/main/java/vista/IVista.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author isabe
+ */
+public interface IVista {
+
+    void actualizarEstado(String mensaje);
+
+    void mostrarError(String error);
+
+    void setVisible(boolean visible);
+}
