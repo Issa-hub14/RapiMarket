62714a328f88463a23eb1e182a70c4cef6c4127d
Issa-hub14 <isabellaquijano1@gmail.com>
2026-05-04 15:34:40 -0500

    Creacion de la interfaz del modelo IModelo

---
+++ src/main/java/modelo/IModelo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author isabe
+ */
+import java.util.List;
+
+public interface IModelo {
+
+    List<Producto> buscarProductos(String termino);
+
+    List<Producto> obtenerCatalogo();
+
+    Carrito obtenerCarrito();
+
+    List<String> obtenerListaDeMercado();
+
+    void agregarAListaMercado(String item);
+
+    void eliminarDeListaMercado(String item);
+
+    Producto buscarProductoPorNombre(String nombre);
+}
